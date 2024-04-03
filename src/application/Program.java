2c1c5b27b36a626ef0f115a2abe519d0a513a86d
Pablo Daniel <pablodanielazaraneves2005@gmail.com>
2024-04-03 20:46:53 -0300

    New Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardsystem.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println(77);
+		Position pos = new Position(4, 5);
+		
+		System.out.println(pos);
+		
 	}
 
 }
